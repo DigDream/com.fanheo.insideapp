3184f4ebac12be88a536d94bfae75d2a787e88ac
dubuqingfeng <1135326346@qq.com>
2014-09-20 13:00:18 +1000

    更新一下下

--- src/com/fanheo/insideapp/ui/ListViewActivity.java
+++ src/com/fanheo/insideapp/ui/ListViewActivity.java
@@ -3,9 +3,7 @@ package com.fanheo.insideapp.ui;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -13,32 +11,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import org.apache.http.util.ByteArrayBuffer;
 import org.apache.http.util.EncodingUtils;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.xml.sax.InputSource;
-import org.xml.sax.XMLReader;
 import org.xmlpull.v1.XmlPullParser;
-
-import com.fanheo.insideapp.R;
-import com.fanheo.insideapp.adapter.ListViewAdapter;
-import com.fanheo.insideapp.data.UserPreferences;
-import com.fanheo.insideapp.util.OrderParseXmlService;
-import com.fanheo.insideapp.util.ParseXmlService;
-import com.handmark.pulltorefresh.library.PullToRefreshBase;
-import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
-import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.format.DateUtils;
@@ -49,11 +31,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class ListViewActivity extends Activity {
+import com.fanheo.insideapp.R;
+import com.fanheo.insideapp.adapter.ListViewAdapter;
+import com.fanheo.insideapp.data.UserPreferences;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+public class ListViewActivity extends Activity implements
+		OnItemClickListener {
 	private ListView listView;
 	private ListViewAdapter listViewAdapter;
 	private List<Map<String, Object>> listItems;
@@ -65,13 +58,14 @@ public class ListViewActivity extends Activity {
 			"爱心：世界都有爱。", "鼠标：反应敏捷。", "音乐CD：酷我音乐。" };
 	Document doc;
 	private PullToRefreshListView mPullRefreshListView;
+
+
 	@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
- 		switch (item.getItemId()) {
- 		case Menu.FIRST + 1:  
- 			System.out.println("点击了注销按钮！");
-			//Toast.makeText(ListViewActivity.this, "存在网络", Toast.LENGTH_LONG).show();
-	 		UserPreferences preferences = new UserPreferences();
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case Menu.FIRST + 1:
+			System.out.println("点击了注销按钮！");
+			UserPreferences preferences = new UserPreferences();
 			preferences.init(ListViewActivity.this);
 			preferences.setAutoLogin(true);
 			Intent intent = new Intent();
@@ -79,22 +73,27 @@ public class ListViewActivity extends Activity {
 			startActivity(intent);
 			finish();
 			break;
- 		}
- 		return super.onOptionsItemSelected(item);
- 	}
-    @Override
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-    	menu.add(Menu.NONE, Menu.FIRST + 1, 5, "注销");
+		menu.add(Menu.NONE, Menu.FIRST + 1, 5, "注销");
 		return true;
 	}
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_listview);
+		final View view = View.inflate(this, R.layout.activity_listview, null);
+		setContentView(view);
+		
 		// loadData();
 		mPullRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_refresh_list);
 		// Set a listener to be invoked when the list should be refreshed.
+
 		mPullRefreshListView
 				.setOnRefreshListener(new OnRefreshListener<ListView>() {
 					@Override
@@ -123,16 +122,49 @@ public class ListViewActivity extends Activity {
 		// android.R.layout.simple_list_item_1, mListItems);
 
 		// listView = (ListView)findViewById(R.id.pull_refresh_list);
-		File file = new File("/sdcard/ver.xml");	
+		File file = new File("/sdcard/ver.xml");
 		listItems = getListItems(file);
 		listViewAdapter = new ListViewAdapter(this, listItems); // 创建适配器
-	        
+
 		// 这两个绑定方法用其一
 		// 方法一
 		// mPullRefreshListView.setAdapter(mAdapter);
 		// 方法二
+		
 		ListView actualListView = mPullRefreshListView.getRefreshableView();
+		registerForContextMenu(actualListView); 
 		actualListView.setAdapter(listViewAdapter);
+		actualListView.setOnItemClickListener(new OnItemClickListener() {
+	
+
+			private Cursor cursor1;
+
+			public void onItemClick(AdapterView<?> arg0, View arg1,
+					int position, long id) {
+				
+				
+			   
+				Toast.makeText(ListViewActivity.this, "test", Toast.LENGTH_LONG)
+						.show();
+				Intent intent = new Intent(getApplicationContext(),
+						OrderInfoActivity.class);
+			    //intent.putExtra("itemInfo", title);
+				ListViewActivity.this.startActivity(intent);
+			}
+
+		});
+		actualListView
+				.setOnItemLongClickListener(new OnItemLongClickListener() {
+					@Override
+					public boolean onItemLongClick(AdapterView<?> arg0,
+							View arg1, int arg2, long arg3) {
+						// When clicked, show a toast with the TextView text
+						
+						Toast.makeText(ListViewActivity.this, "您不小心长按了一下",
+								Toast.LENGTH_LONG).show();
+						return false;
+					}
+				});
 		// listView.setAdapter(listViewAdapter);
 	}
 
@@ -172,125 +204,101 @@ public class ListViewActivity extends Activity {
 	}
 
 	/**
-	 * 初始化商品信息
+	 * 初始化订单信息
 	 */
 	private List<Map<String, Object>> getListItems(File file) {
-		if(file == null)
-            return null;
-        XmlPullParser parser = Xml.newPullParser();
-        List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
+		if (file == null)
+			return null;
+		XmlPullParser parser = Xml.newPullParser();
+		List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
 		Map<String, Object> map = new HashMap<String, Object>();
-       /* List<Map<string,string>> list = null;
-        Map<string,string> map = null;*/
-        try
-        {
-            parser.setInput(new FileInputStream(file),"utf-8");
-             
-            int type = parser.getEventType();
-            while(type != XmlPullParser.END_DOCUMENT)
-            {
-                switch (type)
-                {
-                case XmlPullParser.START_TAG:
-                    if("info".equals(parser.getName()))
-                    {
-                    	listItems = new ArrayList<Map<String,Object>>();
-                    }else if("city".equals(parser.getName()))
-                    {
-                        map = new HashMap<String, Object>();
-                        map.put("title",parser.getAttributeValue(null,"name"));
-                        System.out.println(parser.getAttributeValue(null,"name"));
-                    }else if("weather".equals(parser.getName()))
-                    {
-                       
-                       // map.put("detail", parser.nextText()); // 物品详情
-                        String hash = parser.nextText();  
-                        map.put("info",hash);
-                        System.out.println(hash);
-                        map.put("image", imgeIDs[2]);
-                    }/*else if("temp".equals(parser.getName()))
-                    {
-                        map.put("temp",parser.nextText());
-                    }else if("wind".equals(parser.getName()))
-                    {
-                        map.put("wind",parser.nextText());
-                    }*/
-                    break;
-                case XmlPullParser.END_TAG:
-                    if("city".equals(parser.getName()))
-                    {
-                    	listItems.add(map);
-                        map = null;
-                    }
-                    break;
-                     
-                default:
-                    break;
-                }
-                type = parser.next();
-            }
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        return listItems;
-		/*List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
-		Map<String, Object> map = new HashMap<String, Object>();
-		// 把version.xml放到网络上，然后获取文件信息
-		InputStream inStream = OrderParseXmlService.class.getClassLoader().getResourceAsStream("ver.xml");
-		// 解析XML文件。 由于XML文件比较小，因此使用DOM方式进行解析
-		OrderParseXmlService service = new OrderParseXmlService();
+		/*
+		 * List<Map<string,string>> list = null; Map<string,string> map = null;
+		 */
 		try {
-			map = service.parseXml(inStream);
+			parser.setInput(new FileInputStream(file), "utf-8");
+
+			int type = parser.getEventType();
+			while (type != XmlPullParser.END_DOCUMENT) {
+				switch (type) {
+				case XmlPullParser.START_TAG:
+					if ("info".equals(parser.getName())) {
+						listItems = new ArrayList<Map<String, Object>>();
+					} else if ("city".equals(parser.getName())) {
+						map = new HashMap<String, Object>();
+						map.put("title", parser.getAttributeValue(null, "name"));
+						System.out.println(parser.getAttributeValue(null,
+								"name"));
+					} else if ("weather".equals(parser.getName())) {
+
+						// map.put("detail", parser.nextText()); // 物品详情
+						String hash = parser.nextText();
+						map.put("info", hash);
+						System.out.println(hash);
+						map.put("image", imgeIDs[2]);
+					}/*
+					 * else if("temp".equals(parser.getName())) {
+					 * map.put("temp",parser.nextText()); }else
+					 * if("wind".equals(parser.getName())) {
+					 * map.put("wind",parser.nextText()); }
+					 */
+					break;
+				case XmlPullParser.END_TAG:
+					if ("city".equals(parser.getName())) {
+						listItems.add(map);
+						map = null;
+					}
+					break;
+
+				default:
+					break;
+				}
+				type = parser.next();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		for (int i = 0; i < goodsNames.length; i++) {
-			Map<String, Object> map = new HashMap<String, Object>();
-			// 把version.xml放到网络上，然后获取文件信息
-			InputStream inStream = OrderParseXmlService.class.getClassLoader().getResourceAsStream("version.xml");
-			// 解析XML文件。 由于XML文件比较小，因此使用DOM方式进行解析
-			OrderParseXmlService service = new OrderParseXmlService();
-			try
-			{
-				mHashMap = service.parseXml(inStream);
-			} catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-			map.put("image", imgeIDs[i]); // 图片资源
-			map.put("title", "物品名称："); // 物品标题
-			map.put("info", goodsNames[i]); // 物品名称
-			map.put("detail", goodsDetails[i]); // 物品详情
-			listItems.add(map);
-		}
-		return listItems;*/
-	}
-	/*private List<Map<String, Object>> getListItems() {
-		List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
-		for (int i = 0; i < goodsNames.length; i++) {
-			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("image", imgeIDs[i]); // 图片资源
-			map.put("title", "物品名称："); // 物品标题
-			map.put("info", goodsNames[i]); // 物品名称
-			map.put("detail", goodsDetails[i]); // 物品详情
-			listItems.add(map);
-		}
 		return listItems;
-	}*/
+		/*
+		 * List<Map<String, Object>> listItems = new ArrayList<Map<String,
+		 * Object>>(); Map<String, Object> map = new HashMap<String, Object>();
+		 * // 把version.xml放到网络上，然后获取文件信息 InputStream inStream =
+		 * OrderParseXmlService
+		 * .class.getClassLoader().getResourceAsStream("ver.xml"); // 解析XML文件。
+		 * 由于XML文件比较小，因此使用DOM方式进行解析 OrderParseXmlService service = new
+		 * OrderParseXmlService(); try { map = service.parseXml(inStream); }
+		 * catch (Exception e) { e.printStackTrace(); } for (int i = 0; i <
+		 * goodsNames.length; i++) { Map<String, Object> map = new
+		 * HashMap<String, Object>(); // 把version.xml放到网络上，然后获取文件信息 InputStream
+		 * inStream =
+		 * OrderParseXmlService.class.getClassLoader().getResourceAsStream
+		 * ("version.xml"); // 解析XML文件。 由于XML文件比较小，因此使用DOM方式进行解析
+		 * OrderParseXmlService service = new OrderParseXmlService(); try {
+		 * mHashMap = service.parseXml(inStream); } catch (Exception e) {
+		 * e.printStackTrace(); } map.put("image", imgeIDs[i]); // 图片资源
+		 * map.put("title", "物品名称："); // 物品标题 map.put("info", goodsNames[i]); //
+		 * 物品名称 map.put("detail", goodsDetails[i]); // 物品详情 listItems.add(map);
+		 * } return listItems;
+		 */
+	}
 
-	/**
-	 * 点击事件
-	 * 
-	 * @author Administrator
-	 * 
+	/*
+	 * private List<Map<String, Object>> getListItems() { List<Map<String,
+	 * Object>> listItems = new ArrayList<Map<String, Object>>(); for (int i =
+	 * 0; i < goodsNames.length; i++) { Map<String, Object> map = new
+	 * HashMap<String, Object>(); map.put("image", imgeIDs[i]); // 图片资源
+	 * map.put("title", "物品名称："); // 物品标题 map.put("info", goodsNames[i]); //
+	 * 物品名称 map.put("detail", goodsDetails[i]); // 物品详情 listItems.add(map); }
+	 * return listItems; }
 	 */
-	class ClickEvent implements OnClickListener {
-
-		@Override
-		public void onClick(View v) {
-		}
-
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+			long id) {
+		Log.d("长按", "test");
+		Toast.makeText(ListViewActivity.this, "test", Toast.LENGTH_LONG).show();
+		Intent intent = new Intent(getApplicationContext(),
+				OrderInfoActivity.class);
+		ListViewActivity.this.startActivity(intent);
 	}
 
 	/**
---
+++ src/com/fanheo/insideapp/ui/OrderInfoActivity.java
@@ -0,0 +1,21 @@
+package com.fanheo.insideapp.ui;
+
+import com.fanheo.insideapp.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderInfoActivity extends Activity {
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_orderinfo);
+	
+	}
+
+}
